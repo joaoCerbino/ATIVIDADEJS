27d86abba933fb5d087d3bcf834c7b18ede96495
JoaoCerbino <jonguicerbinonho@gmail.com>
2026-09-15 16:22:15 -0400

    sada

---
+++ Desafio55/script.js
@@ -0,0 +1,34 @@
+var saldo = 0
+var ow = true
+do{
+    let sele = parseInt(prompt("1 - Consultar saldo \
+        2 - Depositar \
+        3 - Sacar \
+        0 - Sair "))
+    
+    switch (sele){
+        case 1:
+            console.log("Seu saldo é de ",saldo)
+        break;
+
+        case 2:
+            let Deposita = parseInt(prompt("Quantos Reais deseja depositar?"))
+            saldo += Deposita
+            console.log("Seu saldo agr é de",saldo)
+            break;
+        case 3:
+            let Sacar = parseInt(prompt("Quantos Reais deseja Sacar?"))
+            console.log(saldo -= Sacar)
+            console.log("Seu saldo agr é de",saldo)
+        break;
+
+        case 0:
+            ow = false
+            break;
+
+        default:
+            console.log("seleção Errada")
+
+    }
+        
+}while(ow)
